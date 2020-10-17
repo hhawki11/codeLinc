1a764e32c1cf2dc68964ac153ab46c6159d88c83
Hunter <hhawki11@uncc.edu>
2020-10-17 16:00:34 -0400

    Add card functionality

--- src/Components/Flashcards/DeckPreview.jsx
+++ src/Components/Flashcards/DeckPreview.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import  { displayDeck } from "../Main/Review"
 import Card from "./Card";
 import getCardData from "./cardsGet";
+import postData from "./cardPost";
 
 function loadCards(card) {
     return (
@@ -20,9 +21,26 @@ function loadCards(card) {
 
 function DeckPreview(props) {
     const [cards, setCards] = useState([]);
+    const [term, setTerm] = useState("");
+    const [def, setDef] = useState("");
 
     function loadDeck() {
         getCardData(props.id).then(response => response.json().then(data => setCards(data)));
+    }
+
+    function setsTerm(event) {
+        setTerm(event.target.value);
+    }
+    function setsDef(event) {
+        setDef(event.target.value);
+    }
+
+    function addNewCard() {
+        postData({
+            "term" : term,
+            "definition" : def,
+            "deckId" : props.id
+        }).then(response => response.json()).then(data => setCards(data));  
     }
 
     return(
@@ -32,6 +50,19 @@ function DeckPreview(props) {
                 <h2 className="previewHeader" >{props.name}</h2>
             </div>
             <h3 className="previewFooter">{props.description}</h3>
+            <input 
+                onChange={setsTerm}
+                type="text"
+                placeholder="Term"
+                className="writeMessage"
+            />
+            <input 
+                onChange={setsDef}
+                type="text"
+                placeholder="Definition"
+                className="writeMessage"
+            />
+            <button onClick={addNewCard} className="sendMessage">Add</button>
             <dl>{cards.map(loadCards)}</dl>
         </div>
     );
---
+++ src/Components/Flashcards/cardPost.js
@@ -0,0 +1,16 @@
+async function postData(data) {
+    return await fetch('http://localhost:8080/rest/cards/addCard', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+}
+
+export default postData;
